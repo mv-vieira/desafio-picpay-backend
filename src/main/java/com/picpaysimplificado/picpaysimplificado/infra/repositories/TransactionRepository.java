ffa4ba412eb2e8a4cefd800932c9af36c0997798
Matheus Vicente <mv_vieira96@hotmail.com>
2024-03-15 12:26:09 -0300

    feat: create Repositories

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/infra/repositories/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.picpaysimplificado.picpaysimplificado.infra.repositories;
+
+import com.picpaysimplificado.picpaysimplificado.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
