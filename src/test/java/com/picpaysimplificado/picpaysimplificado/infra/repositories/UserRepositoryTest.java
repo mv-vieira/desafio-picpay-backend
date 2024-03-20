3012ebb6ddcdef38820eaa5bc655f1e2535d0112
Matheus Vicente <mv_vieira96@hotmail.com>
2024-03-20 14:22:49 -0300

    chore: remove urls

---
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/infra/repositories/UserRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.picpaysimplificado.picpaysimplificado.infra.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Test
+    void findUserByDocument() {
+    }
+}
